4cc4de57b2fb151077069cdd6526789f61afadc2
Ankit Soni <dev@ankitson.com>
2023-12-07 19:40:21 -0800

    bugfix: __init__ typo in DB class

--- database.py
+++ database.py
@@ -6,18 +6,15 @@ import config
 # so we reuse the same connection across all users of this class
 class Database():
     connection = None
-    def _init__(self):
+    def __init__(self):
         if Database.connection is None:
             Database.connection = sqlite3.connect(config.settings.db.filename)
-        cur = Database.connection.cursor() 
-        cur.execute("""CREATE TABLE IF NOT EXISTS runs
-            (user TEXT, code TEXT, day INTEGER, part INTEGER, time REAL, answer INTEGER, answer2)""")
-        cur.execute("""CREATE TABLE IF NOT EXISTS solutions
-            (key TEXT, day INTEGER, part INTEGER, answer INTEGER, answer2)""")
-        Database.connection.commit()
+            cur = Database.connection.cursor()
+            cur.execute("""CREATE TABLE IF NOT EXISTS runs
+                (user TEXT, code TEXT, day INTEGER, part INTEGER, time REAL, answer INTEGER, answer2)""")
+            cur.execute("""CREATE TABLE IF NOT EXISTS solutions
+                (key TEXT, day INTEGER, part INTEGER, answer INTEGER, answer2)""")
+            Database.connection.commit()
 
     def get(self):
-        if not Database.connection:
-            self._init__()
         return Database.connection
-    
