fcb1319d6af11e6da7b03688e9f8f4e3175f604b
proegssilb <proegssilb@gmail.com>
2023-12-09 21:26:19 -0500

    fix: Container wouldn't build in QA.

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ from dynaconf import Dynaconf, Validator
 
 settings = Dynaconf(
     envvar_prefix="FERRIS_ELF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=[
+        'settings.toml', 
+        '.secrets.toml',
+        '/srv/ferris-elf/config/settings.toml',
+        '/srv/ferris-elf/config/.secrets.toml'
+        ],
     validators=[
         Validator("discord.bot_token",must_exist=True)
     ]
