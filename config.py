602be1354169c8318ffdad9ac97ab735dd3b4827
proegssilb <proegssilb@gmail.com>
2023-12-19 19:08:59 -0500

    fix: Enable custom configs for QA/Prod envs

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os.path
+
 from dynaconf import Dynaconf, Validator
+
+CONF_DIR = os.path.join(os.path.expanduser("~"), ".config", "ferris-elf")
 
 settings = Dynaconf(
     envvar_prefix="FERRIS_ELF",
-    settings_files=["settings.toml", ".secrets.toml"],
+    settings_files=["settings.toml", ".secrets.toml", os.path.join(CONF_DIR, "settings.toml"), os.path.join("secrets.toml")],
     validators=[Validator("discord.bot_token", must_exist=True)],
 )
 
