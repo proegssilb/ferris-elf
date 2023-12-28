38269fe5465b97aaeb58786dcca660166228a84e
proegssilb <proegssilb@gmail.com>
2023-12-28 11:41:59 -0500

    fix: Make constants use info from config files (#53)
    * fix: Enable custom configs for QA/Prod envs
    * fix: Make constants use data from config files
    * fix: Add more config validators

--- config.py
+++ config.py
@@ -1,9 +1,29 @@
+import os.path
+
 from dynaconf import Dynaconf, Validator
+
+CONF_DIR = os.path.join(os.path.expanduser("~"), ".config", "ferris-elf")
 
 settings = Dynaconf(
     envvar_prefix="FERRIS_ELF",
-    settings_files=["settings.toml", ".secrets.toml"],
-    validators=[Validator("discord.bot_token", must_exist=True)],
+    merge_enable=True,
+    settings_files=[
+        "settings.toml",
+        ".secrets.toml",
+        os.path.join(CONF_DIR, "settings.toml"),
+        os.path.join(CONF_DIR, "secrets.toml"),
+    ],
+    validators=[
+        Validator("discord.bot_token", must_exist=True),
+        Validator("discord.owner_id", must_exist=True, cast=int),
+        Validator("db.filename", must_exist=True),
+        Validator("discord.support_info", must_exist=True),
+        Validator("discord.rust_version_info", must_exist=True),
+        Validator("discord.hw_info", must_exist=True),
+        Validator("aoc.inputs_dir", must_exist=True),
+        Validator("docker.container_ref", must_exist=True),
+        Validator("aoc_auth.tokens", must_exist=True, len_min=1),
+        ],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
