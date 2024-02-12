fa13418575181964711a49f1c5f19e2797ff7fd6
proegssilb <proegssilb@gmail.com>
2024-02-12 12:20:32 -0500

    style: Remove annotations that CI mypy doesn't want.

--- tests/test_picoseconds.py
+++ tests/test_picoseconds.py
@@ -6,13 +6,11 @@ import hypothesis.strategies as st
 from ferris_elf.picoseconds import Picoseconds
 
 
-# Given is untyped.
-@given(st.integers())  # type: ignore[misc]
+@given(st.integers())
 def test_picos_id(x: int) -> None:
     assert Picoseconds.from_picos(x).as_picos() == x
 
 
-# Given is untyped.
 @given(
     st.floats(
         min_value=-9e300,
@@ -21,10 +19,10 @@ def test_picos_id(x: int) -> None:
         allow_infinity=False,
         allow_subnormal=False,
     )
-)  # type: ignore[misc]
-@example(9245793277968768.0)  # type: ignore[misc]
-@example(2.814749767114317e16)  # type: ignore[misc]
-@example(-0.53125)  # type: ignore[misc]
+)
+@example(9245793277968768.0)
+@example(2.814749767114317e16)
+@example(-0.53125)
 def test_nanos_id(x: float) -> None:
     # Floating point math rarely produces identical/ideal results.
     # Yes, that really is how far off we can be. Don't lose more than 2 hours trying it out.
@@ -32,7 +30,7 @@ def test_nanos_id(x: float) -> None:
     assert delta <= 0.01
 
 
-# Given is untyped.
+
 @given(
     st.floats(
         min_value=-9e300,
@@ -41,27 +39,24 @@ def test_nanos_id(x: float) -> None:
         allow_infinity=False,
         allow_subnormal=False,
     )
-)  # type: ignore[misc]
+)
 def test_nanos_to_picos(x: float) -> None:
     assert Picoseconds.from_nanos(x).as_picos() == int(x * 1000)
 
 
-# Given is untyped.
-@given(st.integers())  # type: ignore[misc]
+@given(st.integers())
 def test_picos_to_nanos(x: int) -> None:
     assert Picoseconds.from_picos(x).as_nanos() == x / 1000
 
 
-# Given is untyped.
-@given(st.integers())  # type: ignore[misc]
+@given(st.integers())
 def test_format_has_units(x: int) -> None:
     formatted = str(Picoseconds.from_picos(x))
     # 'ps' also ends with 's'.
     assert formatted.endswith("s") or formatted.endswith("m") or formatted.endswith("h")
 
 
-# Given is untyped.
-@given(st.integers(min_value=0, max_value=86_400_000_000_000_000))  # type: ignore[misc]
+@given(st.integers(min_value=0, max_value=86_400_000_000_000_000))
 def test_format_ranges(val: int) -> None:
     formatted = str(Picoseconds.from_picos(val))
     match val:
